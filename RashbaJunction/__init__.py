12dcfced867353c04e1b404d1b1e4e80999f1342
Leon <dd@gmail.com>
2022-08-30 14:31:11 +0200

    Delete deprechated files

--- RashbaJunction/__init__.py
+++ RashbaJunction/__init__.py
@@ -1,5 +1,4 @@
-# from RashbaJunction.RashbaJunction_0_3 import RashbaJunction as RashbaJunction
-from .Errors import EnergyOutOfRangeError, InsulatorError
-
 from RashbaJunction.PureRashba import PureRashba
 from RashbaJunction.RashbaJunction_0_4 import RashbaJunction, WaveFunction
+
+from .Errors import EnergyOutOfRangeError, InsulatorError
